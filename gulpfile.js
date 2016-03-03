d821c6ecbea683f950289d944b61ce5e3fcf155c
nicoladl <nicola.delazzari@gmail.com>
2016-03-03 18:05:53 +0100

    fix path issue

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 var gulp     = require('gulp'),
 sass         = require('gulp-sass'),
 autoprefixer = require('gulp-autoprefixer'),
-uglify       = require('gulp-uglifyjs'),
 gulpif       = require('gulp-if'),
 concat       = require('gulp-concat'),
 minifyCss    = require('gulp-clean-css'),
@@ -16,37 +15,34 @@ browserSync  = require('browser-sync').create();
 
 var prod = false;
 
-var basepath = './src/',
-webPath      = basepath + 'web',
+var basePath = './src',
+webPath      = './web',
 bowerPath    = './bower_components';
 
 var vendor = [
-	bowerPath + '/jquery/dist/jquery.min.js'
+    bowerPath + '/jquery/dist/jquery.min.js'
 ]
 
 // task - bower vendors
 gulp.task('vendor', function(){
 
-	return gulp.src(vendor)
-	    .pipe(uglify({
-            preserveComments: 'some'
-        }))
-	    .pipe(concat('plugins.js'))
-	    .pipe(gulp.dest(webPath + '/js/vendor'))
+    return gulp.src(vendor)
+        .pipe(concat('plugins.js'))
+        .pipe(gulp.dest(webPath + '/js/vendor'))
 });
 
 // task - sass
 gulp.task('sass', function () {
 
-  return gulp.src(basepath + 'sass/**/*.scss')
+  return gulp.src(basePath + '/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-	.pipe(autoprefixer({
-		browsers: ['last 2 versions'],
-		cascade: false
-	}))
-	.pipe(gulpif(prod, minifyCss({
-		compatibility: 'ie8'}
-	)))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulpif(prod, minifyCss({
+        compatibility: 'ie8'}
+    )))
     .pipe(gulp.dest(webPath + '/css'))
     .pipe(browserSync.stream());
 });
@@ -54,18 +50,15 @@ gulp.task('sass', function () {
 // task - js
 gulp.task('js',function(){
 
-	return gulp.src(basepath + 'js/*.js')
-	    .pipe(gulpif(prod, uglify({
-            preserveComments: 'some'
-        })))
-	    .pipe(concat('script.js'))
-		.pipe(gulp.dest(webPath + '/js'));
+    return gulp.src(basePath + '/js/*.js')
+        .pipe(gulpif(prod, concat('script.js')))
+        .pipe(gulp.dest(webPath + '/js'));
 });
 
 // task - image minificator
 gulp.task('images', function() {
 
-    return gulp.src(basepath + 'img/*')
+    return gulp.src(basePath + '/img/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
@@ -76,7 +69,7 @@ gulp.task('images', function() {
 
 // task - w3c controller
 gulp.task('w3cjs', function () {
-    return gulp.src(basepath + '*.html')
+    return gulp.src(basePath + '/*.html')
     .pipe(w3cjs())
     .pipe(through2.obj(function(file, enc, cb){
         cb(null, file);
@@ -89,28 +82,36 @@ gulp.task('w3cjs', function () {
 // task - copy files
 gulp.task('copy-files', function(){
 
-	return gulp.src(['index.html', basepath + 'img/**/*', basepath + 'font/*'],{ 'base' : '.' })
-		.pipe(gulp.dest(webPath));
+    return gulp.src(['index.html', basePath + '/img/**/*', basePath + '/font/*'],{ 'base' : '.' })
+        .pipe(gulp.dest(webPath));
 });
 
 // task - production task
 gulp.task('prod',function(){
-	prod = true;
-	runSequence('vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files');
+    prod = true;
+    runSequence('vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files');
 });
 
 // task - watch task
 gulp.task('watch', ['vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files'] ,function(){
 
-	browserSync.init({
+    browserSync.init({
         server: webPath + '/'
     });
 
- 	gulp.watch(basepath + 'sass/**/*.scss', ['sass']).on('change', browserSync.reload);
- 	gulp.watch(basepath + 'js/*.js', ['js']).on('change', browserSync.reload);
- 	gulp.watch(basepath + 'img/**/*', ['images']).on('change', browserSync.reload);
- 	gulp.watch(basepath + '*.html', ['w3cjs', 'copy-files']).on('change', browserSync.reload);
- 	
+
+    gulp.src(basePath + '/font/*')
+        .pipe(gulp.dest(webPath + '/font'));
+
+
+    gulp.src(basePath + '/*.html')
+        .pipe(gulp.dest(webPath + '/'));
+
+    gulp.watch(basePath + '/sass/**/*.scss', ['sass']).on('change', browserSync.reload);
+    gulp.watch(basePath + '/js/*.js', ['js']).on('change', browserSync.reload);
+    gulp.watch(basePath + '/img/**/*', ['images']).on('change', browserSync.reload);
+    gulp.watch(basePath + '/*.html', ['w3cjs', 'copy-files']).on('change', browserSync.reload);
+    
 });
 
 gulp.task('default', ['vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files']);
