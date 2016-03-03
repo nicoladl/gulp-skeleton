e30a170da851a296571f96f6884e37f84be57199
nicoladl <nicola.delazzari@gmail.com>
2016-03-03 16:51:12 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+'use strict';
+ 
+var gulp     = require('gulp'),
+sass         = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+uglify       = require('gulp-uglifyjs'),
+gulpif       = require('gulp-if'),
+concat       = require('gulp-concat'),
+minifyCss    = require('gulp-clean-css'),
+runSequence  = require('run-sequence'),
+imagemin     = require('gulp-imagemin'),
+pngquant     = require('imagemin-pngquant'),
+w3cjs        = require('gulp-w3cjs'),
+through2     = require('through2'),
+browserSync  = require('browser-sync').create();
+
+var prod = false;
+
+var basepath = './',
+webPath      = basepath + 'web',
+bowerPath    = './bower_components';
+
+var vendor = [
+	bowerPath + '/jquery/dist/jquery.min.js'
+]
+
+gulp.task('vendor', function(){
+
+	return gulp.src(vendor)
+	    .pipe(uglify({
+            preserveComments: 'some'
+        }))
+	    .pipe(concat('plugins.js'))
+	    .pipe(gulp.dest(webPath + '/js/vendor'))
+});
+
+gulp.task('sass', function () {
+
+  return gulp.src(basepath + 'sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(gulpif(prod, minifyCss({
+		compatibility: 'ie8'}
+	)))
+    .pipe(gulp.dest(webPath + '/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js',function(){
+
+	return gulp.src(basepath + 'js/*.js')
+	    .pipe(gulpif(prod, uglify({
+            preserveComments: 'some'
+        })))
+	    .pipe(concat('script.js'))
+		.pipe(gulp.dest(webPath + '/js'));
+});
+
+gulp.task('images', function() {
+
+    return gulp.src(basepath + 'img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(webPath + '/img'));
+});
+
+gulp.task('w3cjs', function () {
+    return gulp.src(basepath + '*.html')
+    .pipe(w3cjs())
+    .pipe(through2.obj(function(file, enc, cb){
+        cb(null, file);
+        if (!file.w3cjs.success){
+            // throw new Error('HTML validation error(s) found');
+        }
+    }));
+});
+
+gulp.task('copy-files', function(){
+
+	return gulp.src(['index.html', basepath + 'img/**/*', basepath + 'font/*'],{ 'base' : '.' })
+		.pipe(gulp.dest(webPath));
+});
+
+gulp.task('prod',function(){
+	prod = true;
+	runSequence('vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files');
+});
+
+gulp.task('watch', ['vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files'] ,function(){
+
+	browserSync.init({
+        server: webPath + '/'
+    });
+
+ 	gulp.watch(basepath + 'sass/**/*.scss', ['sass']).on('change', browserSync.reload);
+ 	gulp.watch(basepath + 'js/*.js', ['js']).on('change', browserSync.reload);
+ 	gulp.watch(basepath + 'img/**/*', ['images']).on('change', browserSync.reload);
+ 	gulp.watch(basepath + '*.html', ['w3cjs', 'copy-files']).on('change', browserSync.reload);
+ 	
+});
+
+gulp.task('default', ['vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files']);
---
+++ readme.md
@@ -0,0 +1,11 @@
+/** TODO **/
+
+-> $ npm install
+-> $ bower install
+-> $ gulp watch
+
+/** START DEPLOY **/
+
+-> $ gulp prod
+
+// Folder WEB
