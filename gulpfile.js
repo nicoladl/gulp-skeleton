d88c526ebf3e63c02f011ea27523043cb86fc046
nicoladl <nicola.delazzari@gmail.com>
2016-03-03 18:16:41 +0100

    update deprecated plugin

--- gulpfile.js
+++ gulpfile.js
@@ -99,10 +99,11 @@ gulp.task('watch', ['vendor', 'sass', 'js', 'images', 'w3cjs', 'copy-files'] ,fu
         server: webPath + '/'
     });
 
-
     gulp.src(basePath + '/font/*')
         .pipe(gulp.dest(webPath + '/font'));
 
+    gulp.src(basePath + '/svg/*')
+        .pipe(gulp.dest(webPath + '/svg'));
 
     gulp.src(basePath + '/*.html')
         .pipe(gulp.dest(webPath + '/'));
